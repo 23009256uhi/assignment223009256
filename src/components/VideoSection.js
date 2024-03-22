a1fdfe26ea2b6bfd76614080e446b9bad1b068bb
23009256uhi <23009256uhi@gmail.com>
2024-03-22 12:36:30 +0000

    confused text input  structure and style

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -2,7 +2,17 @@ import "./VideoSection.css";
 import { useState } from "react";
 
 function VideoSection({ onClose }) {
+  const [showInput, setShowInput] = useState(false);
   const [confusedText, setConfusedText] = useState("");
+
+  function handleConfusedClick() {
+    setShowInput(true);
+  }
+
+  function handleCancelClick() {
+    setConfusedText("");
+    setShowInput(false);
+  }
 
   return (
     <>
@@ -15,11 +25,44 @@ function VideoSection({ onClose }) {
           onClick={onClose}
         ></button>
         <div className="video-placeholder" />
-        <div className="text-end">
-          <button type="button" className="btn btn-danger confused-btn">
-            Confused
-          </button>
-        </div>
+        {!showInput && (
+          <div className="text-end">
+            <button
+              type="button"
+              className="btn btn-danger confused-btn"
+              onClick={handleConfusedClick}
+            >
+              Confused
+            </button>
+          </div>
+        )}
+        {showInput && (
+          <div className="confused-form">
+            <h4>What have you found confusing about this video?</h4>
+            <textarea
+              className="form-control"
+              rows="3"
+              value={confusedText}
+              onChange={(e) => setConfusedText(e.target.value)}
+            ></textarea>
+            <div className="button-group">
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={handleConfusedClick}
+              >
+                Send
+              </button>
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={handleCancelClick}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
